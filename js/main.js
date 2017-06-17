1ee0a28d32bd24f806bfcac838f905e767bd07ee
Anabel Sposato <sposatoanabel@gmail.com>
2017-06-17 13:45:37 +0100

    my work section

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 $(document).ready(function () {
 // Initialize and Configure Magnific Popup Lightbox Plugin
     /*$('.popup-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-        }
-    }); // magnificPopup function
-*/
+     delegate: 'a',
+     type: 'image',
+     tLoading: 'Loading image #%curr%...',
+     mainClass: 'mfp-img-mobile',
+     gallery: {
+     enabled: true,
+     navigateByImgClick: true,
+     preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+     },
+     image: {
+     tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+     }
+     }); // magnificPopup function
+     */
 
 
-    $('.popup-gallery').each(function() {
+    $('.popup-gallery').each(function () {
         var $container = $(this);
         var $imageLinks = $container.find('.item');
 
         var items = [];
-        $imageLinks.each(function() {
+        $imageLinks.each(function () {
             var $item = $(this);
             var type = 'image';
 
@@ -37,14 +37,14 @@ $(document).ready(function () {
         $imageLinks.magnificPopup({
             mainClass: 'mfp-fade',
             items: items,
-            gallery:{
-                enabled:true,
+            gallery: {
+                enabled: true,
                 tPrev: $(this).data('prev-text'),
                 tNext: $(this).data('next-text')
             },
             type: 'image',
             callbacks: {
-                beforeOpen: function() {
+                beforeOpen: function () {
                     var index = $imageLinks.index(this.st.el);
                     if (-1 !== index) {
                         this.goTo(index);
@@ -54,18 +54,17 @@ $(document).ready(function () {
         });
 
 
-
-    $imageLinks.magnificPopup({
+        $imageLinks.magnificPopup({
             mainClass: 'mfp-fade',
             items: items,
-            gallery:{
-                enabled:true,
+            gallery: {
+                enabled: true,
                 tPrev: $(this).data('prev-text'),
                 tNext: $(this).data('next-text')
             },
             type: 'image',
             callbacks: {
-                beforeOpen: function() {
+                beforeOpen: function () {
                     var index = $imageLinks.index(this.st.el);
                     if (-1 !== index) {
                         this.goTo(index);
@@ -117,10 +116,97 @@ $(document).ready(function () {
         e.preventDefault();
 
         $('body, html').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
+            scrollTop: $($(this).attr('href')).offset().top
         }, 600)
     }); // jump to section
+    var shuffleme = (function( $ ) {
+        'use strict';
+        var $grid = $('#grid'), //locate what we want to sort
+            $filterOptions = $('.portfolio-sorting li'),  //locate the filter categories
+            $sizer = $grid.find('.shuffle_sizer'),    //sizer stores the size of the items
+
+            init = function() {
+
+                // None of these need to be executed synchronously
+                setTimeout(function() {
+                    listen();
+                    setupFilters();
+                }, 100);
+
+                // instantiate the plugin
+                $grid.shuffle({
+                    itemSelector: '[class*="col-"]',
+                    sizer: $sizer
+                });
+            },
 
 
 
+            // Set up button clicks
+            setupFilters = function() {
+                var $btns = $filterOptions.children();
+                $btns.on('click', function(e) {
+                    e.preventDefault();
+                    var $this = $(this),
+                        isActive = $this.hasClass( 'active' ),
+                        group = isActive ? 'all' : $this.data('group');
+
+                    // Hide current label, show current label in title
+                    if ( !isActive ) {
+                        $('.portfolio-sorting li a').removeClass('active');
+                    }
+
+                    $this.toggleClass('active');
+
+                    // Filter elements
+                    $grid.shuffle( 'shuffle', group );
+                });
+
+                $btns = null;
+            },
+
+            // Re layout shuffle when images load. This is only needed
+            // below 768 pixels because the .picture-item height is auto and therefore
+            // the height of the picture-item is dependent on the image
+            // I recommend using imagesloaded to determine when an image is loaded
+            // but that doesn't support IE7
+            listen = function() {
+                var debouncedLayout = $.throttle( 300, function() {
+                    $grid.shuffle('update');
+                });
+
+                // Get all images inside shuffle
+                $grid.find('img').each(function() {
+                    var proxyImage;
+
+                    // Image already loaded
+                    if ( this.complete && this.naturalWidth !== undefined ) {
+                        return;
+                    }
+
+                    // If none of the checks above matched, simulate loading on detached element.
+                    proxyImage = new Image();
+                    $( proxyImage ).on('load', function() {
+                        $(this).off('load');
+                        debouncedLayout();
+                    });
+
+                    proxyImage.src = this.src;
+                });
+
+                // Because this method doesn't seem to be perfect.
+                setTimeout(function() {
+                    debouncedLayout();
+                }, 500);
+            };
+
+        return {
+            init: init
+        };
+    }( jQuery ));
+
+    $(document).ready(function()
+    {
+        shuffleme.init(); //filter portfolio
+    });
 }); // init
